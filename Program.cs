096f4cd5400596a94a55bbcdb25ad29891bc006a
Karolina <karroliina.k@gmail.com>
2019-09-24 09:38:54 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Clear();
+
+                Console.WriteLine("[1] Add to do");
+                Console.WriteLine("[2] List to do");
+                Console.WriteLine("[3] Exit");
+
+                ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+
+                Console.Clear();
+
+                switch (keyPressed.Key)
+                {
+                    case ConsoleKey.D1: //add to do
+
+                        break;
+
+                    case ConsoleKey.D2: //list to do
+
+                        break;
+
+                    case ConsoleKey.D3: //exit
+
+                        isRunning = false;
+                        break;
+                }
+            }
         }
     }
 }
